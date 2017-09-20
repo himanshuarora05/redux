b304ad7d4249cec0b08a4f8ffb03c76dbc63528d
Himanshu Arora <Himanshu.Arora@iontrading.com>
2017-09-20 16:37:07 +0530

    add todo redux example

--- app/app.jsx
+++ app/app.jsx
@@ -14,3 +14,4 @@ ReactDOM.render(
 );
 
 require('./redux-example.jsx');
+require('./redux-todo-example.jsx');
--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,38 +2,11 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-//pure function
-function add(a , b) {
-  return a + b;
+var reducer = (state = {name: 'Anonymous'}, action) => {
+  return state;
 }
 
-// not a pure function, a could change
-var a = 3;
-function add(b) {
-  return a + b;
-}
+var store = redux.createStore(reducer);
+var currentState = store.getState();
 
-var result;
-function add (a, b) {
-  result = a + b;
-  return result;
-}
-
-function add(a, b) {
-  return a + b + new Date().getSeconds();
-}
-
-
-function changeProp(obj){
-  return {
-    ...obj,
-    name: 'Him'
-  }
-};
-
-var res = changeProp({
-  name: 'himanshu',
-  age: 25
-});
-
-console.log(res);
+console.log(currentState);
