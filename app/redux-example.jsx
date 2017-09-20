17f42563b9051f3b2955cd7e6885c9a8c2e70895
Himanshu Arora <Himanshu.Arora@iontrading.com>
2017-09-20 16:58:16 +0530

    Add support for Change_name

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,10 +3,26 @@ var redux = require('redux');
 console.log('Starting redux example');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
-  return state;
+  console.log('New Action', action);
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+      break;
+    default:
+
+  }
 }
 
 var store = redux.createStore(reducer);
 var currentState = store.getState();
 
 console.log(currentState);
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Himanshu'
+});
+console.log(store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -7,9 +7,28 @@ var stateDefault = {
   showCompleted: false,
   todos: []
 }
+
 var reducer = (state = stateDefault, action) => {
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      }
+      break;
+    default:
+
+  }
   return state;
 }
 
 var store = redux.createStore(reducer);
+console.log(store.getState());
+
+var action = {
+  type: "CHANGE_SEARCH_TEXT",
+  searchText: "work"
+}
+
+store.dispatch(action);
 console.log(store.getState());
